d75651c17fdfd7e462de20cf0c95eb7dd91af326
sonu <sonu@gmail.com>
2024-07-14 12:51:25 +0530

    adding jenkinfile

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,10 +1,12 @@
 pipeline{
     agent any 
+    
     stages{
         stage('stage-1'){
             steps{
                 script{
-                    println 'My name is:' + params.name 
+                    println 'My name is: ' + params.name 
+                    println 'My srname is: ' + params.surname  
                 }
             }
         }

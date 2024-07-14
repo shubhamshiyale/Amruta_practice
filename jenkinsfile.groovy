08365e4012fda224068f2cd6826e0316622cb30b
sonu <sonu@gmail.com>
2024-07-14 13:24:04 +0530

    adding jenkinfile

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -6,6 +6,10 @@ pipeline{
             steps{
                 script{
                     println 'My name is: ' + params.name
+                    def people_name = params.name.split(',')
+                    for (element in people_name) {
+                        println 'My name is: ' + element
+                    }
                     //name = "shubham,amruta,pallavi"
                     // def people_names = params.name.split(',') 
                     // for(value in people_names){
